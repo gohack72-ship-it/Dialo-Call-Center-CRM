4f250f7d8aeda5ca3e1c7ef96e266adbe6506ff7
Finiya <finiya.tp@gmail.com>
2026-01-23 14:22:42 +0530

    pulling constant files-finiya

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dialo/addlead.dart';
 import 'package:dialo/splash%20screen.dart';
 import 'package:flutter/material.dart';
 // 👈 import your page here
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home:AnimatedSplashScreenWidget(), // 👈 this is your home page
+      home:NewLeadPage(), // 👈 this is your home page
       debugShowCheckedModeBanner: false,
     );
   }
