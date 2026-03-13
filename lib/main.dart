78b99373b962635f6bcac37def22b7845aaf230b
Shifa <shifakt44@gmail.com>
2026-03-13 10:12:48 +0530

    pushime theme changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-
-
-
-
 import 'package:dialo/views/Name_page.dart';
 import 'package:dialo/views/addlead.dart';
 import 'package:dialo/views/bottomnavigationbar.dart';
@@ -11,6 +7,7 @@ import 'package:dialo/views/leads_screen.dart';
 import 'package:dialo/views/reportpage.dart';
 import 'package:dialo/views/reportsum.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
@@ -20,8 +17,33 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
+@override
+void initState(){
+  super.initState();
+  loadTheme();
+}
+void loadTheme()async{
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  setState(() {
+    isDarkMode = prefs.getBool('isDarkMode') ?? false;
+  });
+}
+void changeTheme(bool value)async {
+SharedPreferences prefs = await SharedPreferences.getInstance();
+await prefs.setBool('isDarkMode', value);
+
+setState(() {
+  isDarkMode = value;
+});}
 
   @override
   Widget build(BuildContext context) {
@@ -29,10 +51,38 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Leads CRM',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+        brightness: Brightness.light,
+        primaryColor: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+        ),
+
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
       ),
-      home: Splashscreen(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blue,
+
+        scaffoldBackgroundColor: const Color(0xff121212),
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff1E1E1E),
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      themeMode: isDarkMode?ThemeMode.dark:ThemeMode.light,
+      home: BottomnavPage(changeTheme: changeTheme,),
     );
   }
 }
--- lib/views/dashboard.dart
+++ lib/views/dashboard.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../constants/app_colors.dart';
 import '../constants/app_textstyle.dart';
 
 class Dashboard extends StatefulWidget {
-  const Dashboard({super.key});
+  final Function(bool) changeTheme;
+  const Dashboard({super.key,required this.changeTheme});
 
   @override
   State<Dashboard> createState() => _DbState();
@@ -29,17 +31,17 @@ class _DbState extends State<Dashboard> {
           ),
         ),
 
-        title: Align(
-          alignment: Alignment.centerRight,
-          child: const Text(
-            "HOME PAGE",
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
-          ),
-        ),
+        // title: Align(
+        //   alignment: Alignment.centerRight,
+        //   child: const Text(
+        //     "HOME PAGE",
+        //     style: TextStyle(
+        //       color: Colors.black,
+        //       fontWeight: FontWeight.bold,
+        //       fontSize: 18,
+        //     ),
+        //   ),
+        // ),
         actions: [
           PopupMenuButton<String>(
             icon: const Icon(Icons.tune, color: Colors.black, size: 25),
@@ -63,7 +65,7 @@ class _DbState extends State<Dashboard> {
           ),
         ],
       ),
-      drawer: const SettingsDrawer(),
+      drawer: SettingsDrawer(changeTheme:widget.changeTheme),
       body: SingleChildScrollView(
         child: Container(
           width: MediaQuery.of(context).size.width,
@@ -306,8 +308,42 @@ class FollowUpCard extends StatelessWidget {
   }
 }
 
-class SettingsDrawer extends StatelessWidget {
-  const SettingsDrawer({super.key});
+class SettingsDrawer extends StatefulWidget {
+  final Function(bool)changeTheme;
+  const SettingsDrawer({super.key,required this.changeTheme});
+  @override
+  State<SettingsDrawer> createState() => _SettingsDrawerState();
+
+  static Widget _item(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(icon, size: 22, color: AppColors.textColor),
+      title: Text(title),
+      onTap: () {},
+    );
+  }
+}
+
+class _SettingsDrawerState extends State<SettingsDrawer> {
+  bool isDarkMode = false;
+
+  @override
+  void initState(){
+    super.initState();
+    loadTheme();
+  }
+
+  void loadTheme()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      isDarkMode = prefs.getBool('isDarkMode') ?? false;
+    });
+  }
+
+  void saveTheme(bool value)async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool("darkMode", value);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -351,21 +387,25 @@ class SettingsDrawer extends StatelessWidget {
             ),
             SizedBox(height: 10),
             const Divider(),
-            _item("Notifications", Icons.notifications),
-            _item("Appearance", Icons.palette),
-            _item("Help & About", Icons.help),
-            _item("Logout", Icons.logout),
+            SettingsDrawer._item("Notifications", Icons.notifications),
+           ListTile(
+             leading: const Icon(Icons.dark_mode,
+                 size: 22,color: AppColors.textColor,),
+             title: const Text("Mode Change"),
+             trailing: Switch(value: isDarkMode,
+                 activeColor: AppColors.themeColor,
+                 onChanged: (value){
+               setState((){
+                 isDarkMode=value;
+               });
+               widget.changeTheme(value);
+                 }),
+           ),
+            SettingsDrawer._item("Help & About", Icons.help),
+            SettingsDrawer._item("Logout", Icons.logout),
           ],
         ),
       ),
-    );
-  }
-
-  static Widget _item(String title, IconData icon) {
-    return ListTile(
-      leading: Icon(icon, size: 22, color: AppColors.textColor),
-      title: Text(title),
-      onTap: () {},
     );
   }
 }
