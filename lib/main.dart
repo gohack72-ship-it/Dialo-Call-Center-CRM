68984d50b5b63757693c438fef37d3ffacd1421f
Shifa <shifakt44@gmail.com>
2026-04-02 12:22:33 +0530

    shifa - pulling lead provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-
-
-
-
 import 'package:dialo/providers/leadProvider.dart';
 
-import 'package:dialo/views/bottomnavigationbar.dart';
+// import 'package:dialo/views/bottomnavigationbar.
+import 'package:dialo/views/leads/addlead.dart';
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -91,7 +88,7 @@ setState(() {
           ),
         ),
         themeMode: isDarkMode?ThemeMode.dark:ThemeMode.light,
-        home: BottomnavPage(changeTheme: changeTheme,),
+        home: NewLeadPage(),
       ),
     );
   }
